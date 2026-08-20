d78d8d41577a1f80d86ff09c3ed85c1444572ad1
Zulfiqar Teams <zulfiqar00778866@gmail.com>
2026-08-20 15:07:56 +0500

    Revert "Update GitHub Actions workflow for portfolio deployment"
    This reverts commit 710e780c9a3bc80e848cfe5ea91a85486b6c99fb.

--- PakUrdu/vite.config.ts
+++ PakUrdu/vite.config.ts
@@ -4,6 +4,7 @@ import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Portfolio_Website/PakUrdu/",   // ← ye line add karein
   plugins: [react()],
   resolve: {
     alias: {
